b2f76e4ecb448398b0f79e65b49a1869880a9e34
Mridul Gupta <mgvit2021@gmail.com>
2020-09-03 20:32:29 +0530

    Updates

---
+++ precache-manifest.9d54d73d3c875f72514f938adfc6411e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "302603db8c5436ca247836203c274e9c",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "71696952ea8298899c4a",
+    "url": "/covid-19-tracker/static/css/main.8447abe3.chunk.css"
+  },
+  {
+    "revision": "e82de9f997148d803eed",
+    "url": "/covid-19-tracker/static/js/2.7f35805c.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/covid-19-tracker/static/js/2.7f35805c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71696952ea8298899c4a",
+    "url": "/covid-19-tracker/static/js/main.ea570d25.chunk.js"
+  },
+  {
+    "revision": "5bdbd92167a555c8ec71",
+    "url": "/covid-19-tracker/static/js/runtime-main.81159ed7.js"
+  },
+  {
+    "revision": "6ce9074d039177a5b09ef64551377167",
+    "url": "/covid-19-tracker/static/media/covid19_logo.6ce9074d.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/covid-19-tracker/precache-manifest.8eeaa31b79775930c4e8c071201f4769.js"
+  "/covid-19-tracker/precache-manifest.9d54d73d3c875f72514f938adfc6411e.js"
 );
 
 self.addEventListener('message', (event) => {
